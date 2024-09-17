b426ecf9bf92b4b60c834b98ea837b551ecd36df
Jose117053 <adolfo19420@gmail.com>
2024-09-17 15:52:14 -0600

    segunda ventana

--- app/src/main/java/com/example/aplicaciondecoches/Menu.java
+++ app/src/main/java/com/example/aplicaciondecoches/Menu.java
@@ -1,9 +1,6 @@
 package com.example.aplicaciondecoches;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,32 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Menu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        // Configurar la escucha de WindowInsets
+        setContentView(R.layout.activity_menu);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        // Obtener referencia al botón con id: button
-        Button loginButton = findViewById(R.id.button);
-
-        // Agregar listener para navegar hacia MenuActivity
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Crear un Intent para iniciar MenuActivity
-                Intent intent = new Intent(MainActivity.this, Menu.class);
-                startActivity(intent);
-            }
         });
     }
 }
